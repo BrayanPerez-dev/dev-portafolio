05e68b3e1d922435d8671eb6596b0c7a92999cce
Brayan-Perez-Martinez <pbryantsam@gmail.com>
2021-11-29 18:36:11 -0600

    updating the data projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ export const projects = [
     desc: "Challenge about a small version of Airbnb in dev-challenges,bootstrap and and desing",
     github: "https://github.com/BrayanPerez-dev/Widnb-app",
     demo: "https://react-widnb-app.netlify.app/",
-    image: "https://imagizer.imageshack.com/img923/4162/n7hvCa.png",
+    image: "https://res.cloudinary.com/dq6ccwero/image/upload/v1637963803/proyects/n7hvCa_ti7k1f.png",
   },
   {
     id: uuidv4(),
@@ -15,7 +15,7 @@ export const projects = [
     desc: "challenge about a todo app with react-redux-ducks in dev-challenges and and desing",
     github: "https://github.com/BrayanPerez-dev/todo-app",
     demo: "https://redux-tododucks-app.netlify.app/",
-    image: "https://imagizer.imageshack.com/img923/2774/LRQxwB.png",
+    image: "https://res.cloudinary.com/dq6ccwero/image/upload/v1637963802/proyects/LRQxwB_rilnht.png",
   },
   {
     id: uuidv4(),
@@ -23,7 +23,7 @@ export const projects = [
     desc: "challenge about use a api with current values of citys weather around the world,context api and and desing",
     github: "https://github.com/BrayanPerez-dev/weather-app",
     demo: "https://react-context-weather-app.netlify.app/",
-    image: "https://imagizer.imageshack.com/img924/9620/zTGqJk.png",
+    image: "https://res.cloudinary.com/dq6ccwero/image/upload/v1637963803/proyects/zTGqJk_zmzavk.png",
   },
   {
     id: uuidv4(),
@@ -31,14 +31,22 @@ export const projects = [
     desc: "React context ,chat app with firebase authentication and bootstrap ",
     github: "https://github.com/BrayanPerez-dev/React-Firebase-Chat",
     demo: "https://react-rffj2x.stackblitz.io/",
-    image: "https://imagizer.imageshack.com/img924/5631/5oAsxB.png",
+    image: "https://res.cloudinary.com/dq6ccwero/image/upload/v1637963802/proyects/5oAsxB_kfcdxk.png",
   },
   {
     id: uuidv4(),
     title: "Thinking About React",
     desc: "Thinking About React with hooks and material-ui",
     github: "https://github.com/BrayanPerez-dev/thinking-about-react",
-    demo: "https://react-ybdtny.stackblitz.io",
-    image: "https://res.cloudinary.com/dq6ccwero/image/upload/v1635986689/Captura_de_pantalla_2170_mndrzb.png",
+    demo: "https://react-ybdtny.stackblitz.io/",
+    image: "https://res.cloudinary.com/dq6ccwero/image/upload/v1635986689/proyects/Captura_de_pantalla_2170_mndrzb.png",
+  },
+  {
+    id: uuidv4(),
+    title: "Quotes Generator",
+    desc: "another challenge  antd desing and styled componets in react",
+    github: "https://github.com/BrayanPerez-dev/quote-generator",
+    demo: "https://notorious-quotes-generator.netlify.app/",
+    image: "https://res.cloudinary.com/dq6ccwero/image/upload/v1638222645/proyects/Captura_de_pantalla_2172_d37sgu.png"
   }
 ]
